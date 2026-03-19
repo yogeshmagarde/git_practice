5b15b48a61f3d2ae2d1b07baae5f85d392313407
Yogeshmagarde <yogeshmagarde1994@gmail.com>
2026-03-19 18:53:56 +0530

    fourth commit

--- demo.py
+++ demo.py
@@ -11,6 +11,27 @@ def greet_user(name):
 def add_numbers(a, b):
     return a + b
 
+
+class Calculator:
+    def __init__(self, name):
+        self.name = name
+
+    def multiply(self, a, b):
+        return a * b
+
+    def divide(self, a, b):
+        if b == 0:
+            return "Cannot divide by zero"
+        return a / b
+
+
+def greet_user(name):
+    return f"Hello {name}, welcome to feature branch!"
+
+
 if __name__ == "__main__":
+    calc = Calculator("Basic Calculator")
+
     print(greet_user("Yogesh"))
-    print("Sum:", add_numbers(10, 20))
+    print("Multiply:", calc.multiply(5, 4))
+    print("Divide:", calc.divide(10, 2))
