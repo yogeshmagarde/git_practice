a0c61364ccdbf0f32d81bcab30da6ec1773ac2e4
Yogeshmagarde <yogeshmagarde1994@gmail.com>
2026-03-19 18:51:29 +0530

    third comment

--- demo.py
+++ demo.py
@@ -3,3 +3,14 @@ print(s)
 
 def greet(name):
     return f"Hello, {name}!"
+
+
+def greet_user(name):
+    return f"Hello {name}, welcome to feature branch!"
+
+def add_numbers(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    print(greet_user("Yogesh"))
+    print("Sum:", add_numbers(10, 20))
