cb987a1071545017f42ee12941ea76635b32fb20
Yogeshmagarde <yogeshmagarde1994@gmail.com>
2026-04-08 19:00:52 +0530

    FIX

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {
-        "message": "Hello user, you have successfully deployed",
+        "message": "Hello user, you have successfully deployed Ankit",
         "current_time": time.time()
     }
