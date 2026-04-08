26708ff637b17d54f82b7860db880de97785db8d
Yogeshmagarde <yogeshmagarde1994@gmail.com>
2026-04-08 18:08:36 +0530

    update time

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
-
+import time
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello user you have sucessfully deployment"}
+    return {
+        "message": "Hello user, you have successfully deployed",
+        "current_time": time.time()
+    }
